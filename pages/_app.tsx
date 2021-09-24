1ec364415a6e2e87782a387d57d0cb46fec516a7
배건태 <kenta.03.1110@gmail.com>
2021-09-24 15:31:30 +0900

    :sparkles: [Set] _app.tsx MobX 관련 설정 세팅
    _app.tsx에 MobX를 사용하기 위한 세팅 진행.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
+import { Provider } from 'mobx-react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>졸업프로젝트</title>
+      </Head>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  );
 }
 
 export default MyApp;
