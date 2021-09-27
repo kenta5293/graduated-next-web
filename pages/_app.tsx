c6921451572ce68e069a1df7f489c8b1e24f4523
배건태 <kenta.03.1110@gmail.com>
2021-09-27 18:06:39 +0900

    :sparkles: [Add] _app.tsx 메타태그 추가
    _app.tsx에 viewport 메타태그 추가.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
         <title>졸업프로젝트</title>
       </Head>
       <Provider>
