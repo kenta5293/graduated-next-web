4062c9b6af4be23262ae2f04a2fef8fee5a63045
배건태 <kenta.03.1110@gmail.com>
2021-09-27 18:00:10 +0900

    :speech_balloon: [Update] pages/index.tsx의 내용 수정
    pages/index.tsx의 내용 수정.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from 'next/head';
 import Link from 'next/link';
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>졸업프로젝트🏠</title>
-      </Head>
       <div>
         <Link href="/about">
-          <a>About</a>
+          <a>→ About 🥳</a>
         </Link>
       </div>
     </>
